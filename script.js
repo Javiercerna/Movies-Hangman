079df79e552ffe4cb85f2b982991da9442eb3469
Javiercerna <javier.cerna@pucp.pe>
2017-01-16 21:49:28 -0500

    Add win/lose checks, reveal letters

--- script.js
+++ script.js
@@ -3,22 +3,24 @@ var hangman = {
   used_letters: [],
   movie_title: '',
   guessed_title: '',
-  getLettersIndex: function(letter,string) {
-    if (string.indexOf(letter) == -1)
+  findLetterIndexes: function(letter,string) {
+    if (string.toLowerCase().indexOf(letter) == -1)
     {
       return [];
     }
-    return [string.indexOf(letter)];
+    return [string.toLowerCase().indexOf(letter)];
   },
   makeGuess: function(letter) {
     if (this.used_letters.indexOf(letter) != -1)
     {
       console.log('Letter already tried');
+      console.log('Guessed Title: ' + this.guessed_title);
       return;
     }
-    letters_index_found = this.getLettersIndex(letter,this.movie_title);
-    if (letters_index_found.length != 0)
+    letter_indexes_found = this.findLetterIndexes(letter,this.movie_title);
+    if (letter_indexes_found.length != 0)
     {
+      this.revealLetter(letter,letter_indexes_found);
       console.log('Letter in movie title!');
     }
     else
@@ -28,12 +30,31 @@ var hangman = {
       console.log('Remaining lives: ' + this.lives);
     }
 
+    this.checkGameEnd();
+    this.used_letters.push(letter);
+    console.log('Guessed Title: ' + this.guessed_title);
+  },
+  revealLetter: function(letter,letter_indexes) {
+    var temp_title_array = this.guessed_title.split('');
+    letter_indexes.forEach(function(index) {
+      temp_title_array[index] = letter;
+    });
+    this.guessed_title = temp_title_array.join('');
+  },
+  isMovieTitleComplete: function(guessed_title) {
+    return guessed_title.indexOf('_') === -1;
+  },
+  checkGameEnd: function()
+  {
+    if (this.isMovieTitleComplete(this.guessed_title))
+    {
+      console.log('You won!');
+    }
+
     if (this.lives == 0)
     {
       console.log('Game over!');
     }
-
-    this.used_letters.push(letter);
   },
   generateMovieTitle: function() {
     this.movie_title = 'Spectral';
